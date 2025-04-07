4392f5d942032eeb9d65ba1a6b458977c42f7b39
Ahmed İkbal ALPER <ahmed@DESKTOP-M9ABEBC>
2025-04-07 11:25:37 +0300

    build klasörünü .gitignore ile dışladım ve repodan kaldırdım

--- build/CMakeFiles/VideoDeduplicator.dir/VideoDeduplicator_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/VideoDeduplicator.dir/VideoDeduplicator_autogen/mocs_compilation.cpp.o: \
- /home/ahmed/c++/guivideo/build/VideoDeduplicator_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
